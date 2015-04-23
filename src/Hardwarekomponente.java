843243fda85080df51bc80e0eefd433d6258b878
Brian <brian.korduan@t-online>
2015-04-23 10:11:29 +0200

    erstellt

---
+++ src/Hardwarekomponente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netzwerkerfassung;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Brian
+ */
+public class Hardwarekomponente {
+
+    private String bezeichnung;
+    private int inventarNummer;
+    private String beschreibung;
+    private Date beschaffungsDatum = new Date();
+    private Date wartungsDatum = new Date();
+
+}
