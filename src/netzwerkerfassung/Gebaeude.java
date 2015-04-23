def41e2ebf0c0bc62aae711f22d7cccf6f64f043
Marco <Marco@10.96.1.13>
2015-04-23 12:34:17 +0200

    gebauede+raum

---
+++ src/netzwerkerfassung/Gebaeude.java
@@ -0,0 +1,30 @@
+package netzwerkerfassung;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gebaeude {
+
+	String straﬂe;
+	String plz;
+	String ort;
+	int  anz_r‰ume;
+	List<Raum>raeume = new ArrayList<Raum>();
+	
+	public Gebaeude(String straﬂe, String plz, String ort, int anz_raeume) {
+		// TODO Auto-generated constructor stub
+		this.straﬂe = straﬂe;
+		this.plz = plz;
+		this.ort = ort;
+		
+		int count = 100;
+		
+		for(int i = 0; i<=anz_raeume; i++){
+			Raum raum = new Raum(count);
+			raeume.add(raum);
+			count = count+1;
+		}
+	}
+	
+
+}
---
+++ src/netzwerkerfassung/Raum.java
@@ -0,0 +1,12 @@
+package netzwerkerfassung;
+
+public class Raum {
+
+	int raumnummer;
+	
+	public Raum(int raumnummer) {
+		// TODO Auto-generated constructor stub
+		this.raumnummer = raumnummer;
+	}
+
+}
