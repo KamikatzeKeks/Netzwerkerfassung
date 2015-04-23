7a63fe74b25165b84e2217699070b5b21abb05c8
davfro <davisfro@hotmail.com>
2015-04-23 10:44:32 +0200

    CSVWriter

---
+++ src/netzwerkerfassung/CSVWriter.java
@@ -0,0 +1,14 @@
+package netzwerkerfassung;
+
+public class CSVWriter {
+
+	public CSVWriter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
