13666aedd1ff99690a0396cad2ae9e2dfd03fe23
davfro <davisfro@hotmail.com>
2015-04-24 22:34:17 +0200

    CSVReadWrite liefert jetzt eine Liste von Komponenten.

--- src/netzwerkerfassung/CSVReadWrite.java
+++ src/netzwerkerfassung/CSVReadWrite.java
@@ -17,30 +17,37 @@ public class CSVReadWrite {
 	private static final String NEW_LINE = "\n";
 		
 	//CSV file header
-	private static final String FILE_HEADER = "id,bezeichnung,Gebäude,Raum";
+	private static final String FILE_HEADER = "Bezeichnung,KomponentenTyp,Gebäude,Raum";
 	
 
 	public CSVReadWrite() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Komponente k1 = new PC("Acer PC", "PC", "103", "11111");
-		Komponente k2 = new Notebook("HP Pavilion", "Notebook", "103", "11111");
-		Komponente k3 = new Switch("Cisco C103", "Switch", "103", "11111");
-		Komponente k4 = new Server("Cisco S220", "Server", "103", "11111");
-		
-		List<Komponente> komponenten = new ArrayList<Komponente>();
-		komponenten.add(k1);
-		komponenten.add(k2);
-		komponenten.add(k3);
-		komponenten.add(k4);
-		
-		//writeCSV(komponenten);
-		readCSV();
-	}
+//	main Methode zum testen.
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		
+//		Komponente k1 = new PC("Acer PC", "PC", "103", "11111");
+//		Komponente k2 = new Notebook("HP Pavilion", "Notebook", "103", "11111");
+//		Komponente k3 = new Switch("Cisco C103", "Switch", "103", "11111");
+//		Komponente k4 = new Server("Cisco S220", "Server", "103", "11111");
+//		
+//		List<Komponente> komponenten = new ArrayList<Komponente>();
+//		komponenten.add(k1);
+//		komponenten.add(k2);
+//		komponenten.add(k3);
+//		komponenten.add(k4);
+//		
+//		writeCSV(komponenten);
+//		komponenten = readCSV();
+//		
+//		for (Komponente k : komponenten) 
+//		{
+//			System.out.println(k.getBezeichnung() + ", " + k.getKomponentenTyp() + ", " + k.getGebaeude() + ", " + k.getRaum());
+//		}
+//		
+//	}
 	
 	public static void writeCSV(List<Komponente> komponenten)
 	{
@@ -59,8 +66,6 @@ public class CSVReadWrite {
 			//Write a new student object list to the CSV file
 			for (Komponente k : komponenten) 
 			{
-				fileWriter.append(String.valueOf(count++));
-				fileWriter.append(COMMA_DELIMITER);
 				fileWriter.append(String.valueOf(k.getBezeichnung()));
 				fileWriter.append(COMMA_DELIMITER);
 				fileWriter.append(String.valueOf(k.getKomponentenTyp()));
@@ -91,10 +96,10 @@ public class CSVReadWrite {
 		}
 	}
 	
-	public static void readCSV() 
+	public static List<Komponente> readCSV() 
 	{
 		BufferedReader br;
-		ArrayList<ArrayList> data = new ArrayList<ArrayList>();
+		List<ArrayList> data = new ArrayList<ArrayList>();
 		String readline;
 		String[] splittedLine;
 		
@@ -104,13 +109,13 @@ public class CSVReadWrite {
 			
 			while ((readline = br.readLine()) != null)
 			{
-				ArrayList<String> lines = new ArrayList<String>();
+				List<String> lines = new ArrayList<String>();
 				splittedLine = readline.split(",");
 				for (int x = 0; x < splittedLine.length; x++)
 				{
 					lines.add(splittedLine[x]);
 				}
-				data.add(lines);
+				data.add((ArrayList) lines);
 			}
 		} 
 		catch (IOException e) {
@@ -118,15 +123,22 @@ public class CSVReadWrite {
 			e.printStackTrace();
 		}
 		
+		List<Komponente> komponenten = new ArrayList<Komponente>();
+		
 		for (ArrayList l : data) 
 		{
+			List<String> object = new ArrayList<String>();
+			
 			for (Object s : l) 
 			{
-				System.out.print(s.toString() + ", ");
+				//System.out.print(s.toString() + ", ");
+				object.add(s.toString());
 			}
-			System.out.println();
+			//System.out.println();
+			komponenten.add(new Komponente(object.get(0), object.get(1), object.get(2), object.get(3)));
 		}
 		
+		return komponenten;
 	}
 
 
