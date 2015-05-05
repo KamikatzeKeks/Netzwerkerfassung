003860c26c4b95a2503ea6dc382b56eb5c0c6c2d
Brian <brian.korduan@t-online>
2015-05-05 15:53:01 +0200

    Bugfixing

--- src/netzwerkerfassung/MainForm.java
+++ src/netzwerkerfassung/MainForm.java
@@ -100,7 +100,7 @@ public class MainForm extends JFrame {
 		
 		
 			jCBGebaeude = new JComboBox<>();
-			//readBuildings();
+			readBuildings();
 			jCBGebaeude.addItemListener(e -> jCbGebaeudeItemChanged(e));
 
 
