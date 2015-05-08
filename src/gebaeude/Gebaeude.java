eb7903aff331cd8e58a8b5c12db3bbfd9106f359
KamikatzeKeks <brian.korduan@t-online.de>
2015-05-08 20:54:49 +0200

    Doku

--- src/gebaeude/Gebaeude.java
+++ src/gebaeude/Gebaeude.java
@@ -14,10 +14,9 @@ public class Gebaeude {
 	private String strasse;
 	private String plz;
 	private String ort;
-	private int anz_raeume;
-	private List<Raum> raeume = new ArrayList<Raum>();
-	private List<Raum> list_raeume = new ArrayList<Raum>();
-
+	private int  anz_raeume;
+	private List<Raum>raeume = new ArrayList<Raum>();
+	private List<Raum>list_raeume = new ArrayList<Raum>();
 	public int getAnz_raeume() {
 		return anz_raeume;
 	}
@@ -34,9 +33,8 @@ public class Gebaeude {
 		this.list_raeume = list_raeume;
 	}
 
-	// Raum[] array_raeume;
-	public Gebaeude(String bezeichnung, String strasse, String plz, String ort,
-			int anz_raeume) {
+	//Raum[] array_raeume;
+	public Gebaeude(String bezeichnung, String strasse, String plz, String ort, int anz_raeume) {
 
 		this.bezeichnung = bezeichnung;
 		this.strasse = strasse;
@@ -44,17 +42,16 @@ public class Gebaeude {
 		this.ort = ort;
 		this.anz_raeume = anz_raeume;
 		int count = 1;
-
-		// Raumnummern werden aus der Anzahl der Räume generiert
-		for (int i = 0; i < anz_raeume; i++) {
+		
+		//Raumnummern werden aus der Anzahl der Räume generiert
+		for(int i = 0; i<anz_raeume; i++){
 			raeume.add(new Raum(count));
-			count ++;
+			count++;
 		}
 
 	}
-
-	public Gebaeude(String bezeichnung, String strasse, String plz, String ort,
-			List<Raum> list_raeume) {
+	
+	public Gebaeude(String bezeichnung, String strasse, String plz, String ort, List<Raum>list_raeume) {
 		this.bezeichnung = bezeichnung;
 		this.strasse = strasse;
 		this.plz = plz;
@@ -62,64 +59,66 @@ public class Gebaeude {
 		this.list_raeume = list_raeume;
 		this.raeume = list_raeume;
 	}
-
+	
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
-
 	public int getAnzRaeume() {
 		return anz_raeume;
 	}
-
+	
 	public String getStrasse() {
 		return strasse;
 	}
-
+	
 	public String getPlz() {
 		return plz;
 	}
-
+	
 	public String getOrt() {
 		return ort;
 	}
-
+	
 	public List<Raum> getRaeume() {
 		return raeume;
 	}
-
 	public List<Raum> getListRaeume() {
 		return list_raeume;
 	}
-
-	public void setBezeichnung(String bez) {
+	public void setBezeichnung (String bez) {
 		this.bezeichnung = bez;
 	}
-
+	
 	public void setStrasse(String strasse) {
 		this.strasse = strasse;
-
+	
 	}
-
+	
 	public void setPlz(String plz) {
 		this.plz = plz;
 	}
-
+	
 	public void setOrt(String ort) {
 		this.ort = ort;
 	}
-
-	public void setAnzRaeume(int anz_raeume) {
+	
+	public void setAnzRaeume(int anz_raeume){
 		this.anz_raeume = anz_raeume;
 	}
-
-	public void setRaeume(List<Raum> raeume) {
+	
+	public void setRaeume(List<Raum>raeume) {
 		this.raeume = raeume;
 	}
-
+	
 	@Override
 	public String toString() {
-
+		
+		
 		return bezeichnung;
 	}
-
+	
 }
+
+	
+
+
--- src/readerWriter/CSVReadWrite.java
+++ src/readerWriter/CSVReadWrite.java
@@ -1,13 +1,13 @@
 package readerWriter;
 
 /**
-* Klasse CSVReadWrite.java
-* Liest und schreibt Daten aus den CSV Dateien
-* data.csv (Komponenten) sowie Gebäude.csv (Gebäude und Räume)
-* 
-* @author Davis Fröse & Marco D'Addona
-* @date	April/Mai 2015
-*/
+ * Klasse CSVReadWrite.java
+ * Liest und schreibt Daten aus den CSV Dateien
+ * data.csv (Komponenten) sowie Gebäude.csv (Gebäude und Räume)
+ * 
+ * @author Davis Fröse & Marco D'Addona
+ * @date	April/Mai 2015
+ */
 
 import gebaeude.Gebaeude;
 import gebaeude.Raum;
@@ -23,77 +23,72 @@ import java.util.List;
 
 import komponenten.Komponente;
 
-
 /**
- * Diese Klasse speichert Komponente in einer CSV datei und kann diese auch lesen. Zudem kann sie auch
- * Gebaeude speichern und lesen.Sie bietet außerdem Methoden zum löschen und Erstellen von CSV Dateien
+ * Diese Klasse speichert Komponente in einer CSV datei und kann diese auch
+ * lesen. Zudem kann sie auch Gebaeude speichern und lesen.Sie bietet außerdem
+ * Methoden zum löschen und Erstellen von CSV Dateien
  * 
  * 
  * @author Davis Fröse
- * @author Marco D'Addonna 
+ * @author Marco D'Addonna
  *
  */
 public class CSVReadWrite {
-	
-	//Delimiter used in CSV file
-	private static final String COMMA_DELIMITER = ",";
-	
-	//Konstruktor
-	public CSVReadWrite()
-	{
-		
-	}
-	
 
-	public static boolean doesFileExist(String fileName)
-	{
+	// Delimiter used in CSV file
+	private static final String COMMA_DELIMITER = ",";
+
+	// Konstruktor
+	public CSVReadWrite() {
+
+	}
+
+	public static boolean doesFileExist(String fileName) {
 		File csvFile = new File(fileName);
-		
-		if(csvFile.exists() == true ){
+
+		if (csvFile.exists() == true) {
 			return true;
-		}else{
+		} else {
 			return false;
 		}
-		
+
 	}
-	
-	public static void createFile(String fileName){
+
+	public static void createFile(String fileName) {
 		new File(fileName);
-		
 
 	}
 
-	public static void deleteFile(String fileName){
+	public static void deleteFile(String fileName) {
 		File file = new File(fileName);
 		file.delete();
 	}
-	
+
 	/**
-	 * Erstellt eine CSV Datei mit den Geräten und schreibt neue Geräte in diese Datei
-	 * (data.csv)
+	 * Erstellt eine CSV Datei mit den Geräten und schreibt neue Geräte in diese
+	 * Datei (data.csv)
+	 * 
 	 * @author Davis Fröse
-	 * @param ArrayList <Komponente> komponenten
+	 * @param ArrayList
+	 *            <Komponente> komponenten
 	 * @return void
 	 */
-	
-	public static void writeCsvGeraete(List<Komponente> komponenten)
-	{
-		
+
+	public static void writeCsvGeraete(List<Komponente> komponenten) {
+
 		FileWriter fileWriter = null;
-				
+
 		try {
 			fileWriter = new FileWriter("data.csv");
 
-				
-//			
-//			//Write the CSV file header
-//			fileWriter.append(FILE_HEADER.toString());
-//			
-//			//Add a new line separator after the header
-//			
+			//
+			// //Write the CSV file header
+			// fileWriter.append(FILE_HEADER.toString());
+			//
+			// //Add a new line separator after the header
+			//
 
-			for (Komponente k : komponenten) 
-			{
+			for (Komponente k : komponenten) {
 				fileWriter.append(String.valueOf(k.getBezeichnung()));
 				fileWriter.append(COMMA_DELIMITER);
 				fileWriter.append(String.valueOf(k.getKomponentenTyp()));
@@ -102,44 +97,44 @@ public class CSVReadWrite {
 				fileWriter.append(COMMA_DELIMITER);
 				fileWriter.append(String.valueOf(k.getRaum()));
 				fileWriter.append(System.lineSeparator());
-			}			
-			
-			//System.out.println("CSV file was created successfully !!!");
-			
+			}
+
+			// System.out.println("CSV file was created successfully !!!");
+
 		} catch (Exception e) {
 			System.out.println("Error in CsvFileWriter !!!");
 			e.printStackTrace();
 		} finally {
-			
+
 			try {
 				fileWriter.flush();
 				fileWriter.close();
 			} catch (IOException e) {
-				System.out.println("Error while flushing/closing fileWriter !!!");
-                e.printStackTrace();
+				System.out
+						.println("Error while flushing/closing fileWriter !!!");
+				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
+
 	/**
 	 * Erstellt eine CSV Datei mit den Gebäuden sowie den Räumen eines Gebäudes
 	 * 
 	 * @author Marco D'Addona
-	 * @param ArrayList <Komponente> komponenten
+	 * @param ArrayList
+	 *            <Komponente> komponenten
 	 * @return void
 	 */
-	
-	public static void writeCSVGebaeude(List<Gebaeude> gebaeude)
-	{
-		
+
+	public static void writeCSVGebaeude(List<Gebaeude> gebaeude) {
+
 		FileWriter fileWriter = null;
-				
+
 		try {
 			fileWriter = new FileWriter("Gebaeude.csv");
 
-			for (Gebaeude g : gebaeude) 
-			{
+			for (Gebaeude g : gebaeude) {
 				fileWriter.append(String.valueOf(g.getBezeichnung()));
 				fileWriter.append(COMMA_DELIMITER);
 				fileWriter.append(String.valueOf(g.getStrasse()));
@@ -148,187 +143,173 @@ public class CSVReadWrite {
 				fileWriter.append(COMMA_DELIMITER);
 				fileWriter.append(String.valueOf(g.getOrt()));
 				fileWriter.append(COMMA_DELIMITER);
-				
+
 				int count = 0;
 				// Raum-Objekte werden durch [ ] in der CSV Datei markiert
-				for(Raum r:g.getRaeume()) {
-					System.out.println(g.getAnzRaeume());
-					if(count == 0) {
+				for (Raum r : g.getRaeume()) {
+					// System.out.println(g.getAnzRaeume());
+					if (count == 0) {
 						fileWriter.append("[");
 					}
-					count ++;
-					
+					count++;
+
 					fileWriter.append(String.valueOf(r.getRaumnummer()));
 					fileWriter.append(COMMA_DELIMITER);
-					
-				
-					//System.out.println(r);
+
+					// System.out.println(r);
 				}
 				fileWriter.append("]");
 				fileWriter.append(System.lineSeparator());
 			}
-			
 
-			
-			
-			System.out.println("CSV file was created successfully !!!");
-			
+			System.out.println("CSV file was created updated !!!");
+
 		} catch (Exception e) {
 			System.out.println("Error in CsvFileWriter !!!");
 			e.printStackTrace();
 		} finally {
-			
+
 			try {
 				fileWriter.flush();
 				fileWriter.close();
 			} catch (IOException e) {
-				System.out.println("Error while flushing/closing fileWriter !!!");
-                e.printStackTrace();
+				System.out
+						.println("Error while flushing/closing fileWriter !!!");
+				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
+
 	/**
-	 * Liest die Datei data.csv (komponenten) aus,
-	 * erstellt Komponenten-Objekte aus den Daten der CSV 
-	 * und erstellt eine ArrayList<Komponente> aus den entsprechenden Daten
+	 * Liest die Datei data.csv (komponenten) aus, erstellt Komponenten-Objekte
+	 * aus den Daten der CSV und erstellt eine ArrayList<Komponente> aus den
+	 * entsprechenden Daten
 	 * 
 	 * @author Davis Fröse
-	 * @param 
+	 * @param
 	 * @return List<Komponenten>
 	 */
-	
-	public static List<Komponente> readCSV() 
-	{
+
+	public static List<Komponente> readCSV() {
 		BufferedReader br;
 		List<ArrayList> data = new ArrayList<ArrayList>();
 		String readline;
 		String[] splittedLine;
-		
-		try
-		{
+
+		try {
 			br = new BufferedReader(new FileReader("data.csv"));
-			
-			while ((readline = br.readLine()) != null)
-			{
+
+			while ((readline = br.readLine()) != null) {
 				List<String> lines = new ArrayList<String>();
 				splittedLine = readline.split(",");
-				for (int x = 0; x < splittedLine.length; x++)
-				{
+				for (int x = 0; x < splittedLine.length; x++) {
 					lines.add(splittedLine[x]);
 				}
 				data.add((ArrayList) lines);
 			}
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			System.out.println("Komponenten.csv nicht vorhanden");
 		}
-		
+
 		List<Komponente> komponenten = new ArrayList<Komponente>();
-		
-		for (ArrayList l : data) 
-		{
+
+		for (ArrayList l : data) {
 			List<String> object = new ArrayList<String>();
-			
-			for (Object s : l) 
-			{
-				//System.out.print(s.toString() + ", ");
+
+			for (Object s : l) {
+				// System.out.print(s.toString() + ", ");
 				object.add(s.toString());
 			}
-			//System.out.println();
-			komponenten.add(new Komponente(object.get(0), object.get(1), object.get(2), object.get(3)));
+			// System.out.println();
+			komponenten.add(new Komponente(object.get(0), object.get(1), object
+					.get(2), object.get(3)));
 		}
-		
+
 		return komponenten;
 	}
 
 	/**
-	 * Liest die CSV Datei mit den Komponenten aus, 
-	 * erstellt Gebäude und Raum-Objekte aus den Daten der CSV 
-	 * und erstellt eine ArrayList<Gebaeude> mit den entsprechenden Daten
-	 * Die Raum-Objekete zu jedem Gebäude werden in der CSV Date durch [] gekennzeichnet.
+	 * Liest die CSV Datei mit den Komponenten aus, erstellt Gebäude und
+	 * Raum-Objekte aus den Daten der CSV und erstellt eine ArrayList<Gebaeude>
+	 * mit den entsprechenden Daten Die Raum-Objekete zu jedem Gebäude werden in
+	 * der CSV Date durch [] gekennzeichnet.
 	 * 
 	 * @author Marco D'Addona
-	 * @param 
+	 * @param
 	 * @return List<Gebaeude>
 	 */
-	public static List<Gebaeude> readCSVGebaeude() 
-	{
+	public static List<Gebaeude> readCSVGebaeude() {
 		BufferedReader br;
 		List<ArrayList> data = new ArrayList<ArrayList>();
 		String readline;
 		String[] splittedLine;
-		
-		
-		try
-		{
+
+		try {
 			br = new BufferedReader(new FileReader("Gebaeude.csv"));
-			
-			while ((readline = br.readLine()) != null)
-			{
+
+			while ((readline = br.readLine()) != null) {
 				List<String> lines = new ArrayList<String>();
-				List<Raum>raeume = new ArrayList<Raum>();
-				
+				List<Raum> raeume = new ArrayList<Raum>();
+
 				splittedLine = readline.split(",");
 
-				if(readline.contains("[")) {
-				
-					//String mit den Räumen
-					String string_raeume = readline.substring(readline.indexOf("[")+1, readline.indexOf("]"));
+				if (readline.contains("[")) {
+
+					// String mit den Räumen
+					String string_raeume = readline.substring(
+							readline.indexOf("[") + 1, readline.indexOf("]"));
 
 					String[] split_raeume = string_raeume.split(",");
-					
-					//Generierung der Raum Objekte und abspeichern in eine ArrayList
-					for(int i = 0; i<split_raeume.length; i++) {
+
+					// Generierung der Raum Objekte und abspeichern in eine
+					// ArrayList
+					for (int i = 0; i < split_raeume.length; i++) {
 						raeume.add(new Raum(Integer.parseInt(split_raeume[i])));
 					}
 
 				}
-				
-				//Alles ausser die Räume in die ArrayList lines speichern
-				for (int x = 0; x <= 3; x++)
-				{
+
+				// Alles ausser die Räume in die ArrayList lines speichern
+				for (int x = 0; x <= 3; x++) {
 					lines.add(splittedLine[x]);
 				}
-				//Räume in die ArrayList lines speichern
-				for(int y = 0; y<raeume.size(); y++) {
-					Integer intRaeume = new Integer(raeume.get(y).getRaumnummer());
+				// Räume in die ArrayList lines speichern
+				for (int y = 0; y < raeume.size(); y++) {
+					Integer intRaeume = new Integer(raeume.get(y)
+							.getRaumnummer());
 					lines.add(intRaeume.toString());
 				}
-				//lines.add(count_raeume.toString());
-				
+				// lines.add(count_raeume.toString());
+
 				data.add((ArrayList) lines);
 			}
-		} 
-		catch (IOException e) {
-		System.out.println("Gebaeude.csv nicht vorhanden");
+		} catch (IOException e) {
+			System.out.println("Gebaeude.csv nicht vorhanden");
 		}
-		
+
 		List<Gebaeude> gebaeude = new ArrayList<Gebaeude>();
-		
-		for (ArrayList l : data) 
-		{
+
+		for (ArrayList l : data) {
 			List<String> object = new ArrayList<String>();
-			
-			for (Object s : l) 
-			{
-				//System.out.print(s.toString() + ", ");
+
+			for (Object s : l) {
+				// System.out.print(s.toString() + ", ");
 				object.add(s.toString());
 			}
-			//System.out.println();
-			List<Raum>list_raeume = new ArrayList<Raum>();
-			for(int c = 4; c<object.size(); c++) {
+			// System.out.println();
+			List<Raum> list_raeume = new ArrayList<Raum>();
+			for (int c = 4; c < object.size(); c++) {
 				Raum raum = new Raum(Integer.parseInt(object.get(c)));
 				list_raeume.add(raum);
-				boolean test =true;
+				boolean test = true;
 			}
-			gebaeude.add(new Gebaeude(object.get(0), object.get(1), object.get(2), object.get(3), list_raeume));
-		
+			gebaeude.add(new Gebaeude(object.get(0), object.get(1), object
+					.get(2), object.get(3), list_raeume));
+
 		}
-		
+
 		return gebaeude;
 	}
 
-         
 }
